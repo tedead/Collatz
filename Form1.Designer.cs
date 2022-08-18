36671d254eb5a4f6db4a81294783a36ceccd4979
tedead <34945023+tedead@users.noreply.github.com>
2022-08-18 08:18:44 -0500

    initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Collatz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.txtLoopCount = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(247, 446);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(593, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(162, 381);
+            this.listBox2.TabIndex = 2;
+            // 
+            // picture1
+            // 
+            this.picture1.Location = new System.Drawing.Point(338, 167);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(200, 200);
+            this.picture1.TabIndex = 3;
+            this.picture1.TabStop = false;
+            // 
+            // txtLoopCount
+            // 
+            this.txtLoopCount.Location = new System.Drawing.Point(139, 466);
+            this.txtLoopCount.Name = "txtLoopCount";
+            this.txtLoopCount.Size = new System.Drawing.Size(100, 20);
+            this.txtLoopCount.TabIndex = 4;
+            this.txtLoopCount.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 545);
+            this.Controls.Add(this.txtLoopCount);
+            this.Controls.Add(this.picture1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.TextBox txtLoopCount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Collatz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picture1.Image = null;
+            var halfX = picture1.ClientRectangle.Width / 2;
+            var halfY = picture1.ClientRectangle.Height / 2;
+            int picture1Y = 0;
+
+            for (int howManyTimes = 0; howManyTimes <= Convert.ToInt32(txtLoopCount.Text); howManyTimes++)
+            {
+                int seedRandomNumber = 0;
+
+                Random seedRandom = new Random();
+                seedRandomNumber = seedRandom.Next();
+
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+
+                int number = 0, temp = 0;
+                Random rnd = new Random(seedRandomNumber);
+                number = rnd.Next(1000);
+
+                int dataSetSize = 1000;
+
+                int[] dataSetArray = new int[dataSetSize];
+
+                listBox1.Items.Add("Starting number: " + number);
+
+                for (int x = 0; x < dataSetSize; x++)
+                {
+                    Application.DoEvents();
+
+                    if (number % 2 == 0) //if even
+                    {
+                        dataSetArray[x] = number;
+                        temp = number;
+                        number = (number / 2);
+                        listBox1.Items.Add("(" + temp + " / 2) = " + number);
+                    }
+                    else //number is odd
+                    {
+                        dataSetArray[x] = number;
+                        temp = number;
+                        number = (number * 3) + 1;
+                        listBox1.Items.Add("(" + temp + " * 3) + 1 = " + number);
+                    }
+
+                    listBox1.SelectedIndex = x;
+                }
+
+                int y = 0;
+
+                foreach (int i in dataSetArray)
+                {
+                    drawPoint(picture1Y, i);
+                    //listBox2.Items.Add(i);
+                    //listBox2.SelectedIndex = y;
+                    //y++;
+                }
+                picture1Y += 5; //Move right on picture1 2 pixels
+            }
+        }
+
+        public void drawPoint(int x, int y)
+        {
+            Graphics g = Graphics.FromHwnd(picture1.Handle);
+            SolidBrush brush = new SolidBrush(Color.LimeGreen);
+            Point dPoint = new Point(x, (picture1.Height - y));
+            dPoint.X = dPoint.X - 2;
+            dPoint.Y = dPoint.Y - 2;
+            Rectangle rect = new Rectangle(dPoint, new Size(4, 4));
+            g.FillRectangle(brush, rect);
+            g.Dispose();
+        }
+    }
+}
